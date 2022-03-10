d27234c146e479fde330bf01d4a8177f55f04f30
Mike Hoyt <derukimlh@gmail.com>
2022-03-10 06:48:06 -0800

    rs: refactor doy09 solution to use lib crate Grid2D type

--- rs/src/bin/day09.rs
+++ rs/src/bin/day09.rs
@@ -9,6 +9,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
+type Grid2D = aoc::grid2d::Grid2D<usize>;
+type Grid2DPoint = aoc::grid2d::Grid2DPoint<usize>;
+
 fn parse(data: &str) -> Result<Grid2D, parser::ParseError> {
     parser::parse(&data)
 }
@@ -42,109 +45,6 @@ mod parser {
                 / "\r" "\n"
         }
     }
-}
-
-// Leverage https://github.com/mlhoyt/aoc2020/blob/main/rs/src/bin/day11part2.rs "Layout"
-// 2D grid abstraction.
-
-#[derive(Debug)]
-pub struct Grid2D {
-    grid: Vec<usize>,
-    width: usize,
-    height: usize,
-}
-
-impl Grid2D {
-    pub fn new(rows: &[Vec<usize>]) -> Result<Self, aoc::AocError> {
-        let mut rv = Self {
-            grid: vec![],
-            width: 0,
-            height: 0,
-        };
-
-        for (i, r) in rows.iter().enumerate() {
-            if i == 0 {
-                rv.width = r.len();
-            } else if rv.width != r.len() {
-                return Err(aoc::AocError::new(
-                    format!(
-                        "row {} has length {} which does not match the previous length {}",
-                        i,
-                        r.len(),
-                        rv.width
-                    )
-                    .as_str(),
-                ));
-            }
-
-            r.iter().for_each(|v| rv.grid.push(*v));
-
-            rv.height += 1;
-        }
-
-        Ok(rv)
-    }
-
-    fn get_yx(&self, y: usize, x: usize) -> Option<usize> {
-        match self.yx_to_index(y, x) {
-            None => None,
-            Some(i) => Some(self.grid[i]),
-        }
-    }
-
-    fn index_to_yx(&self, i: usize) -> (usize, usize) {
-        let row = (i / self.width) as usize;
-        let col = (i % self.width) as usize;
-
-        (row, col)
-    }
-
-    fn yx_to_index(&self, y: usize, x: usize) -> Option<usize> {
-        if y < (self.height) && x < (self.width) {
-            let n = (y * self.width) + (x);
-            Some(n)
-        } else {
-            None
-        }
-    }
-
-    fn iter(&self) -> Grid2DIter {
-        Grid2DIter {
-            grid: self,
-            index: 0,
-        }
-    }
-}
-
-struct Grid2DIter<'a> {
-    grid: &'a Grid2D,
-    index: usize,
-}
-
-impl<'a> Iterator for Grid2DIter<'a> {
-    type Item = Grid2DPoint;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.index >= self.grid.grid.len() {
-            None
-        } else {
-            let pos = self.grid.index_to_yx(self.index);
-            self.index += 1;
-
-            Some(Self::Item {
-                x: pos.1,
-                y: pos.0,
-                value: self.grid.get_yx(pos.0, pos.1).unwrap(),
-            })
-        }
-    }
-}
-
-#[derive(Hash, PartialEq, Eq, Clone)]
-struct Grid2DPoint {
-    x: usize,
-    y: usize,
-    value: usize,
 }
 
 fn part1(data: &Grid2D) -> Result<usize, Box<dyn std::error::Error>> {
